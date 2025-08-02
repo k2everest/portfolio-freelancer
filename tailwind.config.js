6eee3d392c28d90dad87699a2c39167dc7440a38
coemoraes <coemoraes@k2everest.com.br>
2025-08-02 20:32:03 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0F172A",
+        accent: "#6366F1",
+        text: "#F1F5F9"
+      }
+    }
   },
   plugins: [],
 }
